ebd2e99c5fd017dce2f79151bb2828cfd59794f8
Pieter O'Hearn <pieter.ohearn@gmail.com>
2019-03-12 09:45:27 +0100

    Finished Checkers Test

--- checkersTest.java
+++ checkersTest.java
@@ -1,5 +1,3 @@
-package Exercise3;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
@@ -7,101 +5,100 @@ import static org.junit.Assert.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class CheckerTest {
-	
+
+	/**
+	 * Testing the movePiece method to determine if the method can successfully
+	 * move a piece in the board.
+	 * @author Pieter
+	 */
 	@Test
 	void testMove() {
 		// create a board to play on and 2 players
 		BlackPlayer p1 = new BlackPlayer("Bob");
 		WhitePlayer p2 = new WhitePlayer("Alice");
-		
+
 		// create a new game
 		Board gameBoard = new Board(p1,p2);
-		
-		String board = "    1 2 3 4 5 6 7 8   <- X axis\n" + 
-				"  +-----------------+\n" + 
-				"1 | b   b   b   b   | 1\n" + 
-				"2 |   b   b   b   b | 2\n" + 
-				"3 | b   b   b   b   | 3\n" + 
-				"4 |                 | 4\n" + 
-				"5 |                 | 5\n" + 
-				"6 |   w   w   w   w | 6\n" + 
-				"7 | w   w   w   w   | 7\n" + 
-				"8 |   w   w   w   w | 8\n" + 
-				"  +-----------------+\n" + 
+
+		String board = "    1 2 3 4 5 6 7 8   <- X axis\n" +
+				"  +-----------------+\n" +
+				"1 | b   b   b   b   | 1\n" +
+				"2 |   b   b   b   b | 2\n" +
+				"3 | b   b   b   b   | 3\n" +
+				"4 |                 | 4\n" +
+				"5 |                 | 5\n" +
+				"6 |   w   w   w   w | 6\n" +
+				"7 | w   w   w   w   | 7\n" +
+				"8 |   w   w   w   w | 8\n" +
+				"  +-----------------+\n" +
 				"    1 2 3 4 5 6 7 8   <- X axis\n";
-		
+
 		assertEquals(board, gameBoard.toString());
-		
+
 		// move
 		Position from = new Position(1,3);
 		Position to = new Position(2,4);
 		gameBoard.movePiece(gameBoard.getSquare(from).getPiece(), to, p1);
-		
-		board = "    1 2 3 4 5 6 7 8   <- X axis\n" + 
-				"  +-----------------+\n" + 
-				"1 | b   b   b   b   | 1\n" + 
-				"2 |   b   b   b   b | 2\n" + 
-				"3 |     b   b   b   | 3\n" + 
-				"4 |   b             | 4\n" + 
-				"5 |                 | 5\n" + 
-				"6 |   w   w   w   w | 6\n" + 
-				"7 | w   w   w   w   | 7\n" + 
-				"8 |   w   w   w   w | 8\n" + 
-				"  +-----------------+\n" + 
+
+		board = "    1 2 3 4 5 6 7 8   <- X axis\n" +
+				"  +-----------------+\n" +
+				"1 | b   b   b   b   | 1\n" +
+				"2 |   b   b   b   b | 2\n" +
+				"3 |     b   b   b   | 3\n" +
+				"4 |   b             | 4\n" +
+				"5 |                 | 5\n" +
+				"6 |   w   w   w   w | 6\n" +
+				"7 | w   w   w   w   | 7\n" +
+				"8 |   w   w   w   w | 8\n" +
+				"  +-----------------+\n" +
 				"    1 2 3 4 5 6 7 8   <- X axis\n";
-		
+
 		assertEquals(board, gameBoard.toString());
 
 	}
 
+	/**
+	 * Testing the possible method to determine if the method can successfully
+	 * determine if a move is possible.
+	 * @author Pieter
+	 */
 	@Test
 	void testPossible() {
 		// create a board to play on and 2 players
 		BlackPlayer p1 = new BlackPlayer("Bob");
 		WhitePlayer p2 = new WhitePlayer("Alice");
 		Board gameBoard = new Board(p1, p2);
-		
 		Position from = new Position(1,3);
 		Position to = new Position(2,4);
-		
-		
-		assertTrue(gameBoard.possible(gameBoard.getSquare(to),gameBoard.getSquare(from).getPiece()));
-
 		Square square = new Square(to);
-		assertTrue(square.isEmpty());
-		
 		Piece piece = new Piece(p1, from);
-		
-		assertTrue(square.getPosition().getY() == piece.getPosition().getY() + 1);
-		assertTrue(piece.getOwner().getColour().equals("Black"));
-		assertTrue(square.getPosition().getX() == piece.getPosition().getX() + 1 || square.getPosition().getX() == piece.getPosition().getX() - 1 );
-		
+
+		assertTrue(gameBoard.possible(gameBoard.getSquare(to),gameBoard.getSquare(from).getPiece()));
 		assertTrue(gameBoard.possible(square,piece));
-		
 	}
-		
-	// ** The getSquare test is not working, not sure why so I decided to move on and not spend more time on it **
-	
+
 	/**
 	 * Testing the getSquare method to determine if the method can successfully return a square from a given position
+	 * @author Jack
 	 */
 	@Test
-	public void testgetSquare() { 
+	public void testgetSquare() {
 		//initialize a game for testing
 		BlackPlayer p1 = new BlackPlayer("Pieter");
 		WhitePlayer p2 = new WhitePlayer("Jack");
-		
-		//Initialize position and square 
+
+		//Initialize position and square
 		Position pos1 = new Position(2,2);
 		Board board = new Board(p1,p2);
-		
+
 		//fails if both objects are not the same
 		assertTrue("getSquare test failed", board.getSquare(pos1).getPosition().equals(pos1));
-		
+
 	}
-	
+
 	/**
 	 * Testing getX and getY to make sure they return coordinates correctly
+	 * @author Jack
 	 */
 	@Test
 	public void testgetCoord() {
@@ -115,70 +112,72 @@ class CheckerTest {
 
 	/**
 	 * Testing that a player cannot successfully move a piece out of bounds.
+	 * @author Jack
 	 */
-	@Test 
+	@Test
 	public void testOutOfBounds() {
-		
+
 		//Initialize an in-bound position to use as reference object to call outOfBounds() for destination coordinates
 		Position inBounds = new Position(1, 1);
-		
+
 		//Testing an in-bounds position -> outOfBounds should return False
 		boolean test1 = inBounds.outOfBounds(3, 5);
 		assertFalse("Out of bounds test failed", test1);
-		
+
 		//Testing an out of bounds position -> outOfBounds should return True
 		boolean test2 = inBounds.outOfBounds(10, 2);
 		assertTrue("Out of bounds test failed", test2);
-		
+
 		//Testing an out of bounds position -> outOfBounds should return True
 		boolean test3 = inBounds.outOfBounds(15, 6);
 		assertTrue("Out of bounds test failed", test3);
-		
+
 		//Testing an out of bounds position -> outOfBounds should return True
 		boolean test4 = inBounds.outOfBounds(9, 9);
 		assertTrue("Out of bounds test failed", test4);
-		
+
 		//Testing an in-bounds position -> outOfBounds should return False
 		boolean test5 = inBounds.outOfBounds(1, 8);
 		assertFalse("Out of bounds test failed", test5);
-		
+
 	}
 
 	/**
 	 * Testing that a player cannot successfully play an invalid move. e.g.
 	 * placing a piece on another coloured square.
+	 * @author Jack
 	 */
 	@Test
 	public void testValidMoves() {
-		
+
 		//initialize a game for testing purposes
 		BlackPlayer p1 = new BlackPlayer("Pieter");
 		WhitePlayer p2 = new WhitePlayer("Jack");
 		Board gameBoard = new Board(p1, p2);
-		
+
 		//initialize parameters for testing possible() for a White Player
 		Position current = new Position(4, 6);
 		Piece base = new Piece(p2, current); //Piece to be passed into possible()
-		
+
 		//Test destination square which is valid
 		Position next1 = new Position(5, 5);
 		Square dest1 = new Square(next1); //destination square to be passed into possible()
 		boolean test1 = gameBoard.possible(dest1, base);
 		assertTrue("Valid moves test failed", test1);
-		
+
 		//Test destination square which is invalid (backward)
 		Position next2 = new Position(5, 7);
 		Square dest2 = new Square(next2); //destination square to be passed into possible()
 		boolean test2 = gameBoard.possible(dest2, base);
 		assertFalse("Valid moves test failed", test2);
-		
+
 		//Test destination square which is invalid (non-diagonal)
 		Position next3 = new Position(4, 5);
 		Square dest3 = new Square(next3); //destination square to be passed into possible()
 		boolean test3 = gameBoard.possible(dest3, base);
 		assertFalse("Valid moves test failed", test3);
-		
-		
+
+
 		//initialize parameters for testing possible() for a Black Player
 		Position currentBl = new Position(7, 3);
 		Piece baseBl = new Piece(p1, currentBl); //Piece to be passed into possible()
@@ -200,11 +199,12 @@ class CheckerTest {
 		Square destBl3 = new Square(nextBl3); //destination square to be passed into possible()
 		boolean testBl3 = gameBoard.possible(destBl3, baseBl);
 		assertFalse("Valid moves test failed", testBl3);
-		
+
 	}
-	
+
 	/**
 	 * Testing that getPosition returns the correct position from a square
+	 * @author Jack
 	 */
 	@Test
 	public void testgetPosition() {
@@ -214,9 +214,10 @@ class CheckerTest {
 		//Verify that getPosition returns the desired position
 		assertSame("getPosition test failed", pos, sq.getPosition());
 	}
-	
+
 	/**
 	 * Testing the isEmpty method to make sure empty squares can be identified
+	 * @author Jack
 	 */
 	@Test
 	public void testisEmpty() {
@@ -224,16 +225,12 @@ class CheckerTest {
 		BlackPlayer p1 = new BlackPlayer("Pieter");
 		WhitePlayer p2 = new WhitePlayer("Jack");
 		Board gameBoard = new Board(p1, p2);
-		
+
 		//Test for a position that is occupied by a piece
 		Position pos1 = new Position(2, 2);
 		assertFalse("isEmpty test failed", gameBoard.getSquare(pos1).isEmpty());
 		//Test for a position that is not occupied by a piece
 		Position pos2 = new Position(4, 4);
 		assertTrue("isEmpty test failed", gameBoard.getSquare(pos2).isEmpty());
-		
 	}
-	
-
-
 }
